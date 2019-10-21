13a4a46f8c3696feef3ed603ccfa093a5b48f1e3
Emma Paris <eparis@atlassian.com>
2019-10-21 12:01:37 +0700

    new file

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyACGX1YqwQETbbf1kgz0k6J7NS3sXzHL0M",
+  authDomain: "fitandcospace-b6186.firebaseapp.com",
+  databaseURL: "https://fitandcospace-b6186.firebaseio.com",
+  projectId: "fitandcospace-b6186",
+  storageBucket: "",
+  messagingSenderId: "797062037947",
+  appId: "1:797062037947:web:a2230dd1830add7b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
